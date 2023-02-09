0184106ece1c81ed6c8967f6668bd67c694ee561
Kirils <kirils@hey.com>
2023-02-09 11:24:09 +0200

    Added: section stress, next btn

--- main.js
+++ main.js
@@ -2,17 +2,16 @@
 let hiddenFormSum = document.querySelector(".form-sum-result");
 let submitBtn = document.querySelector(".submit-btn");
 let nextBtn = document.querySelector(".next-section-btn");
-let prevBtn = document.querySelector(".prev-section-btn");
 let sumYesVal = document.querySelectorAll(".sum-yes-val");
 let sumNoVal = document.querySelectorAll(".sum-no-val");
 let inputRadioAll = document.querySelectorAll("input[type=radio]");
 let fieldsetQuestions = document.querySelectorAll(".questions");
-let sectionEat = document.querySelector(".section-eat");
+let formSection = document.querySelectorAll(".form-section");
+let pageForm = document.querySelector(".page-form");
 
 let resultSum = 0;
 let inputRadioArray = [];
 
-submitBtn.setAttribute("disabled", "");
 nextBtn.setAttribute("disabled", "");
 // prevBtn.setAttribute("disabled", "");
 
@@ -26,6 +25,27 @@ inputRadioAll.forEach(function(inputRadio) {
       }
     })
 }); 
+
+console.log(formSection.length - 1);
+
+nextBtn.addEventListener("click", function() {
+  for (let i = 0; i < formSection.length; i++) {
+    
+    if (formSection[i].classList.contains("is-active")) {
+      formSection[i].classList.remove("is-active")
+      formSection[i].classList.add("is-hidden")
+      
+      formSection[i+1].classList.remove("is-hidden")
+      formSection[i+1].classList.add("is-active")
+      // const elementsArray = Array.from(elements);
+
+      break;
+    } 
+      // nextBtn.classList.add("is-hidden");
+      // submitBtn.classList.remove("is-hidden");
+  }
+});
+
 
 
 // Count all data attributes values for Sum
